fbca64fb9361a0a517a785661bc3db476eba7120
SergeevSezar <sergeev.sezar@yandex.ru>
2021-11-17 06:49:49 +0900

    added binding adapters

---
+++ app/src/main/java/com/example/bookkeeper/presentation/BindingAdapters.kt
@@ -0,0 +1,60 @@
+package com.example.bookkeeper.presentation
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.bookkeeper.R
+import com.example.bookkeeper.domain.entity.GameResult
+
+@BindingAdapter("requiresAnswers")
+fun bindRequiredAnswers(textView: TextView, count: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_answers),
+        count
+    )
+}
+
+@BindingAdapter("scoreAnswers")
+fun bindScoreAnswers(textView: TextView, count: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_answers),
+        count
+    )
+}
+
+@BindingAdapter("requiredPercentage")
+fun bindRequiredPercentage(textView: TextView, count: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_percentage),
+        count
+    )
+}
+
+@BindingAdapter("scorePercentage")
+fun bindScorePercentage(textView: TextView, gameResult: GameResult) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_percentage),
+        getPercentOfRightAnswers(gameResult)
+    )
+}
+
+private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) {
+    if (countOfQuestions == 0) {
+        0
+    } else {
+        ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+    }
+}
+
+@BindingAdapter("resultEmoji")
+fun bindResultEmoji(imageView: ImageView, isWinner: Boolean) {
+    imageView.setImageResource(getSmileResId(isWinner))
+}
+
+private fun getSmileResId(isWinner: Boolean): Int {
+    return if (isWinner) {
+        R.drawable.smile
+    } else {
+        R.drawable.sad
+    }
+}
--- app/src/main/java/com/example/bookkeeper/presentation/GameFinishFragment.kt
+++ app/src/main/java/com/example/bookkeeper/presentation/GameFinishFragment.kt
@@ -30,50 +30,12 @@ class GameFinishFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupClickListeners()
-        bindViews()
+        binding.gameResult = args.gameResult
     }
 
     private fun setupClickListeners() {
         binding.buttonRetry.setOnClickListener {
             retryGame()
-        }
-    }
-
-    private fun bindViews() {
-        with(binding) {
-            emojiResult.setImageResource(getSmileResId())
-            tvRequiredAnswers.text = String.format(
-                getString(R.string.required_answers),
-                args.gameResult.gameSettings.minCountOfRightAnswers
-            )
-            tvScoreAnswers.text = String.format(
-                getString(R.string.score_answers),
-                args.gameResult.countOfRightAnswers
-            )
-            tvRequiredPercentage.text = String.format(
-                getString(R.string.required_percentage),
-                args.gameResult.gameSettings.minPercentOfRightAnswers
-            )
-            tvScorePercentage.text = String.format(
-                getString(R.string.score_percentage),
-                getPercentOfRightAnswers()
-            )
-        }
-    }
-
-    private fun getSmileResId(): Int {
-        return if (args.gameResult.winner) {
-            R.drawable.smile
-        } else {
-            R.drawable.sad
-        }
-    }
-
-    private fun getPercentOfRightAnswers() = with(args.gameResult) {
-        if (countOfQuestions == 0) {
-            0
-        } else {
-            ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
         }
     }
 
